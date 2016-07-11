693e4318f5ca7eac2fad5b5670da72f143609ccb
Rostislav Bagrov <bagrov.rostislav@gmail.com>
2016-07-11 09:53:37 +0300

    Clone the modified repository

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,7 +5,7 @@ if [ "$1" = '/docker-command.sh' ]; then
 
 	# setup minimal config if no config is found
 	if [ ! -f "$FREESWITCH_CONF"/freeswitch.xml ]; then
-		git clone https://github.com/spali/freeswitch_conf_minimal.git "$FREESWITCH_CONF"
+		git clone https://github.com/rbagrov/freeswitch_conf_minimal.git "$FREESWITCH_CONF"
 	fi
 
 	chown -R freeswitch:freeswitch "$FREESWITCH_CONF"
